af23e27e1dd651d6fa8f1b4b4281732dd380b34c
seb-odessa <seb@ukr.net>
2016-11-14 19:21:24 +0200

    renamed variables

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,54 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
 
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage {} WORD DICTIONARY", args[0]);
+    }
+    let word: String = args[1].clone().to_uppercase();
+    println!("{}", &word);
+    let dict = load(&args[2]);
+    for word in &check_dict(&word, &dict) {
+        let word: String = word.clone();
+        println!("{} {}", word, dict[&word]);
+    }
+}
+
 fn load(filename: &String) -> HashMap<String, String> {
-    let mut map: HashMap<String, String> = HashMap::new();
+    let mut dict: HashMap<String, String> = HashMap::new();
     if let Some(file) = File::open(&filename).ok() {
         let mut key = String::with_capacity(32);
         let mut val = String::with_capacity(256);
         let reader = BufReader::new(file);
         for line in reader.lines() {
-            let mut word = true;
+            let mut first = true;
             for ch in line.unwrap().chars() {
-                if word {
+                if first {
                     if ch.is_alphabetic() {
                         key.push(ch);
                     } else {
-                        word = false;
+                        first = false;
                     }
                 } else {
                     val.push(ch);
                 }
             }
             if !key.is_empty() {
-                map.insert(key.clone().to_uppercase(), val.clone());
+                dict.insert(key.clone().to_uppercase(), val.clone());
             }
             key.clear();
             val.clear();
         }
     }
-    return map;
+    return dict;
 }
 
-fn check_word(mut map: HashMap<char, i32>, word: &String) -> bool {
+fn check_word(mut chars: HashMap<char, i32>, word: &String) -> bool {
     let mut word: Vec<char> = word.chars().collect();
     word.sort();
     for ch in word {
-        let entry = map.entry(ch).or_insert(0);
+        let entry = chars.entry(ch).or_insert(0);
         if *entry == 0 {
             return false;
         } else {
@@ -47,7 +61,7 @@ fn check_word(mut map: HashMap<char, i32>, word: &String) -> bool {
     return true;
 }
 
-fn check(word : &String, map: &HashMap<String, String>) -> Vec<String> {
+fn check_dict(word : &String, dict: &HashMap<String, String>) -> Vec<String> {
     let mut words: Vec<String> = Vec::new();
     let mut chars: HashMap<char, i32> = HashMap::new();
     for ch in word.chars() {
@@ -55,24 +69,11 @@ fn check(word : &String, map: &HashMap<String, String>) -> Vec<String> {
         *entry += 1;
     }
 
-    for (key, _) in map {
-        if check_word(chars.clone(), &key) {
-            words.push(key.clone());
+    for (word, _) in dict {
+        if check_word(chars.clone(), &word) {
+            words.push(word.clone());
         }
     }
     return words;
 }
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 3 {
-        println!("Usage {} WORD DICTIONARY", args[0]);
-    }
-    let word: String = args[1].clone().to_uppercase();
-    println!("{}", &word);
-    let map = load(&args[2]);
-    for found in &check(&word, &map) {
-        let word: String = found.clone();
-        println!("{} {}", word, map[&word]);
-    }
-}
