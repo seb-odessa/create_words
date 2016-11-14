eea1f4e2b5699a21bf66b7b956107bbca59a75af
seb-odessa <seb@ukr.net>
2016-11-14 18:29:31 +0200

    Working now

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
 
-fn load(filename: &String) ->Option<HashMap<String, String>> {
+fn load(filename: &String) -> HashMap<String, String> {
     let mut map: HashMap<String, String> = HashMap::new();
     if let Some(file) = File::open(&filename).ok() {
         let mut key = String::with_capacity(32);
@@ -23,27 +23,54 @@ fn load(filename: &String) ->Option<HashMap<String, String>> {
                     val.push(ch);
                 }
             }
-            if !val.is_empty() && !val.is_empty() {
-                map.insert(key.clone(), val.clone());
+            if !key.is_empty() {
+                map.insert(key.clone().to_lowercase(), val.clone());
             }
             key.clear();
             val.clear();
         }
     }
-    Some(map)
+    map
 }
 
+fn check_word(mut word: HashMap<char, i32>, maybe: &String) -> bool {
+    for ch in maybe.chars() {
+        let entry = word.entry(ch).or_insert(0);
+        if *entry == 0 {
+            return false;
+        } else {
+            *entry -= 1;
+        }
+    }
+    return true;
+}
+
+fn check(word : &String, map: &HashMap<String, String>) -> Vec<String> {
+    let mut words: Vec<String> = Vec::new();
+    let mut chars: HashMap<char, i32> = HashMap::new();
+    for ch in word.chars() {
+        let entry = chars.entry(ch).or_insert(0);
+        *entry += 1;
+    }
+
+    for (key, _) in map {
+        if check_word(chars.clone(), &key) {
+            words.push(key.clone());
+        }
+    }
+    return words;
+}
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() != 3 {
         println!("Usage {} WORD DICTIONARY", args[0]);
     }
-
-    if let Some(map) = load(&args[2]) {
-        for (key, val) in &map {
-                println!("{}", key);
-            }
+    let word: String = args[1].clone().to_lowercase();
+    println!("{}", &word);
+    let map = load(&args[2]);
+    for found in &check(&word, &map) {
+        let word: String = found.clone();
+        println!("{} {}", word, map[&word]);
     }
-    println!("{} => :", &args[1]);
 }
